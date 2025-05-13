8eb336d5a544ac8d4862d4cb3d01467b53dc6d49
tiagogrothe <tiagogrothe@gmail.com>
2025-05-13 09:48:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ def home():
     return "Backend OCR + GPT ativo", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
